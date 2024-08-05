31557c50f284cfc2f82ea2e34b4b856dc0dd1e54
gautamkumar1 <gautamraaz936@gmail.com>
2024-08-06 00:01:31 +0530

    Added Frontend url in Backend

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://task-2-blog-website.vercel.app",
   // origin: "*",
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true,
